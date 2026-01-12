9ea34848b60a09608e3d7785ffc80ce4f66bd0ec
Euri Herasme <euri.herasme@gmail.com>
2026-01-12 18:33:21 -0400

    Fix P2P connection inside a web worker

--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,23 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{
-    js_sys::{Function, Promise},
-    window,
+    js_sys::{self, Function, Promise},
+    window, DedicatedWorkerGlobalScope,
 };
 
 pub async fn sleep(ms: i32) {
     let mut callback = |resolve: Function, _| {
-        window()
-            .unwrap()
-            .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, ms)
-            .unwrap();
+        match window() {
+            Some(window) => window
+                .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, ms)
+                .unwrap(),
+            None => {
+                let scope = DedicatedWorkerGlobalScope::from(JsValue::from(js_sys::global()));
+                scope
+                    .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, ms)
+                    .unwrap()
+            }
+        };
     };
 
     let promise = Promise::new(&mut callback);
